45d2a55a4d4fda50fc8c11aeab1617599e6afdf4
ZeyadShawki <zizoelfager14@gmail.com>
2024-12-17 13:36:34 +0200

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/layout/todo_app/home_layout.dart';
-import 'package:flutter_application_1/shared/bloc_observer.dart';
 import 'package:flutter_application_1/shared/cubit/cubit.dart';
 import 'package:flutter_application_1/shared/cubit/states.dart';
-import 'package:flutter_application_1/shared/network/local/chashed_helper.dart';
 import 'package:flutter_application_1/shared/styles/themes.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
   var scaffoldKey = GlobalKey<ScaffoldState>();
